2237f41edc136aecb8eb0225ad071ceed4e4342a
materwelonDhruv <materwelondhruv@gmail.com>
2026-08-07 03:05:25 -0700

    feat: put steps in order

--- cli/create-seedcord/src/index.ts
+++ cli/create-seedcord/src/index.ts
@@ -1,1 +1,4 @@
 export { CAPABILITIES, intentsFor, partialsFor, privilegedFor } from './interview/capabilities';
+export { applyFlags } from './interview/applyFlags';
+export { runFlow } from './interview/runFlow';
+export { STEPS } from './interview/steps';
